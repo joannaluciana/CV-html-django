3aca0e8042cd685b6c34a4394cbfab798e8d3a84
Joanna <joanna@wloskowicz.pl>
2020-10-02 11:06:26 +0200

    cv model and admin model

---
+++ reviews/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Reviews
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    date_hierarchy = ('pub_date')
+    list_display = ('title', 'content', 'state')
+    search_fields = ('title', 'pub_date')
+
+    def krotki_opis(self, obj):
+        return (
+            f'{obj.content[:50]}...'
+            if len(obj.content) > 50
+            else obj.content
+        )
+
+
+
+
+
+admin.site.register(Reviews, ReviewsAdmin)
+
---
+++ reviews/models.py
@@ -0,0 +1,92 @@
+from django.conf import settings
+from django.db import models
+from django.core.validators import MinValueValidator, MaxLengthValidator
+
+from works.models import Project
+
+
+class Reviews(models.Model):
+    # dodaje User.reviews relacje odwrotna autor1.project_set.filter(costam)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        verbose_name='author',
+        help_text='',
+    )
+    title = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name='title',
+        help_text='',
+    )
+    content = models.TextField(
+        max_length=300,
+        null=False,
+        blank=False,
+        verbose_name='Content',
+        help_text='',
+    )
+    pub_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Date of timestamp',
+        help_text='',
+    )
+    STATE_CHOICES = (
+        ('draft', 'Draft'),
+        ('in_moderation', 'In moderation'),
+        ('rejected', 'Rejected'),
+        ('published', 'Published'),
+    )
+
+    state = models.CharField(
+        choices=STATE_CHOICES,
+        max_length=40,
+        null=False,
+        blank=False,
+        verbose_name='title',
+        help_text='',
+    )
+
+    grade = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+        verbose_name='grade',
+        help_text='Validate project from 1 - 10',
+        validators=[
+            MinValueValidator(0),
+            MaxLengthValidator(11),
+        ],
+    )
+
+
+class Grade(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        verbose_name='author',
+        help_text='',
+    )
+
+    grade = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+        verbose_name='grade',
+        help_text='Validate project from 1 - 10',
+        validators=[
+            MinValueValidator(0),
+            MaxLengthValidator(11),
+        ],
+    )
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        verbose_name='Project',
+        help_text='',
+    )
